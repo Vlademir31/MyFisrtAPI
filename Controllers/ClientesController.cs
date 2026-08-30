208862935a1ce0ed6dd46746b919252702c71236
Vlade <vladecassia@gmail.com>
2026-08-30 19:50:19 -0300

    Adiciona Get de clientes com controler e configura OpenAPI no Program

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Model;
+
+namespace MyFirstAPI.Controllers
+{
+    [ApiController]
+    [Route("clientes")]
+    public class ClientesController
+    {
+        private static List<Cliente> clientes = new List<Cliente>
+       {
+        new Cliente
+            {
+        Id = 1,
+        Nome = "Vlademir",
+        Email = "vlade@email.com"
+    },
+
+    new Cliente
+    {
+        Id = 2,
+        Nome = "Maria",
+        Email = "maria@email.com"
+    }
+};
+
+ [HttpGet]
+ public List<Cliente> ObterTodos()
+        {
+            return clientes;
+        }
+
+        
+    }
+}
