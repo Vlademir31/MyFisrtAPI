f8165a2e249f82c3fe0fcb1abafbc395ff08f1e7
Vlade <vladecassia@gmail.com>
2026-09-02 07:38:49 -0300

    Adiciona validação obrigatória para cliente

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyFirstAPI.Model
 {
     public class Cliente
     {
         public int Id { get; set; }
+       [Required]
         public string Nome { get; set; } = string.Empty;
         // = string.Empty(serve para inicializar uma propriedade com uma string vazia)
+        [Required]
         public string Email { get; set; } = string.Empty;
     }
 }
