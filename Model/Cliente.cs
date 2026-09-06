0a846f79236aeb5d3868e3ef19a49f7cb210dac4
Vlade <vladecassia@gmail.com>
2026-09-06 14:19:23 -0300

    Define limites de tamanho dos campos

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -5,10 +5,13 @@ namespace MyFirstAPI.Model
     public class Cliente
     {
         public int Id { get; set; }
+
        [Required]
+       [MaxLength(150)]
         public string Nome { get; set; } = string.Empty;
         // = string.Empty(serve para inicializar uma propriedade com uma string vazia)
         [Required]
+        [MaxLength(254)]
         public string Email { get; set; } = string.Empty;
     }
 }
--- Model/Funcionario.cs
+++ Model/Funcionario.cs
@@ -7,13 +7,23 @@ namespace MyFirstAPI.Model
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(150)]
         public string Nome { get; set; } = string.Empty;
 
         [Required]
+        [MaxLength(14)]
         public string Cpf { get; set; } = string.Empty;
+        
+        [MaxLength(254)]
         public string Email { get; set; } = string.Empty;
+
+        [MaxLength(20)]
         public string Telefone { get; set; } = string.Empty;
+
+        [MaxLength(100)]
         public string Cargo { get; set; } = string.Empty;
+
+        [MaxLength(100)]
         public string Departamento { get; set; } = string.Empty;
          public DateTime DataAdmissao { get; set; } 
         public bool Ativo { get; set; } = true;
